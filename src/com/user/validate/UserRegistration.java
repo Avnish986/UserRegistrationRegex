6927b5ef5dee6b7f4815ee142de04bcf840c2f6e
avnish986 <avnishgupta986@gmail.com>
2020-09-28 18:07:56 +0530

    [Avnish]Added:uc1 first name

--- src/com/user/validate/UserRegistration.java
+++ src/com/user/validate/UserRegistration.java
@@ -1,10 +1,26 @@
 package com.user.validate;
-
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class UserRegistration {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to User registration");
+		System.out.println("Enter First Name");
+		Scanner s=new Scanner(System.in);
+		String fname=s.nextLine();
+		Pattern pattern=Pattern.compile("^[A-Z]{1}[a-z]{2,}");
+		Matcher matcher=pattern.matcher(fname);
+
+		boolean matchFound=matcher.find();
+		if(matchFound)
+		{
+		System.out.println("Valid");
+		}
+		else
+		System.out.println("Invalid");
+		}
 	}
 
-}
+
